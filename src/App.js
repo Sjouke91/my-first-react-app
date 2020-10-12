043d5764f0f985dbc5f3dabbec511f64697462dd
Sjouke91 <s.bosma1991@gmail.com>
2020-10-12 12:32:00 +0200

    Made a hello app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// const React = require("react")
+
+function Hello (){
+  return <h1>Hello...</h1> 
+}
+
+function  Greeting ({ name, age }){
+  const message = age 
+    ? ` You are ${age} years old` 
+    : ` your age is not inputted`
+  return <div>
+    <Hello />
+    {name}!
+    {message}
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greeting name="Sjouke" age="28" />
+      <Greeting name="Erik" age="32" />
     </div>
   );
 }
