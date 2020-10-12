6219bd7bf292c63bf14edaea8cb25fdcc9a3edff
Sjouke91 <s.bosma1991@gmail.com>
2020-10-12 16:53:33 +0200

    mapping over values

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React from "react";
+
 // const React = require("react")
 
-function Hello (){
-  return <h1>Hello...</h1> 
+function Hello() {
+  return <h1>Hello...</h1>;
 }
 
-function  Greeting ({ name, age }){
-  const message = age 
-    ? ` You are ${age} years old` 
-    : ` your age is not inputted`
-  return <div>
-    <Hello />
-    {name}!
-    {message}
-  </div>
+function Greeting({ name, age }) {
+  const message = age
+    ? ` You are ${age} years old`
+    : ` your age is not inputted`;
+  return (
+    <div>
+      <Hello />
+      {name}!{message}
+    </div>
+  );
 }
 
 function App() {
-  return (
-    <div>
-      <Greeting name="Sjouke" age="28" />
-      <Greeting name="Erik" age="32" />
-    </div>
-  );
+  const people = [
+    { name: "Sjouke", age: 28 },
+    { name: "Erik", age: 30 },
+    { name: "Rein", age: 31 },
+    { name: "Lisa", age: 35 },
+    { name: "Tim", age: 27 },
+  ];
+
+  const greetings = people.map((person) => {
+    const greeting = (
+      <Greeting key={person.name} name={person.name} age={person.age} />
+    );
+    return greeting;
+  });
+  return <div>{greetings}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 //require the app.js file
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
